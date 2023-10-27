f9867451122d1369b1073f63e93fc7ac00cd71b8
danielstrauss02 <60661519+danielstrauss02@users.noreply.github.com>
2023-10-27 15:50:50 +0200

    fix minor styling

--- main.cpp
+++ main.cpp
@@ -59,17 +59,19 @@ int main() {
   /*
    * Viertes Kapitel
    */
-  std::cout << "Der Prinz und der Frosch" << std::endl
-            << std::endl;
+  std::cout << "Der Prinz und der Frosch" 
+            << std::endl << std::endl;
   std::cout << "Vor langer Zeit gab es einmal einen magischen See, der war so "
                "besonders, dass er nur von denjenigen gefunden werden konnte, "
                "von denen der See sich finden lassen wollte."
             << std::endl
             << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
                           // unter diesem Satz
-   std::cout << "Dieser See war wunderschön, denn er war perfekt in jeder Hinsicht." << std::endl;
+   std::cout << "Dieser See war wunderschön, denn er war perfekt in jeder Hinsicht." 
+             << std::endl;
    std::cout << "Ein Prinz war sich der Schönheit dieses Sees bewusst, und spielte mit dem Gedanken" <<
-                "eines Tage eine Prinzessin hier zu heiraten." << std::endl;
+                "eines Tage eine Prinzessin hier zu heiraten." 
+             << std::endl;
    std::cout << "Eines Tages ging der Prinz des Landes an den See, um zu baden."
              << std::endl
              << std::endl;
