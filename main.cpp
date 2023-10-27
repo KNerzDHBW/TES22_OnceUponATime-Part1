aeee966fcaaae24c49cdba9e95c6132d56e6116e
flo-sem <es22015@lehre.dhbw-stuttgart.de>
2023-10-27 15:05:24 +0200

    "Kapitel4/Satz2"

--- main.cpp
+++ main.cpp
@@ -55,6 +55,7 @@ int main() {
             << std::endl
             << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
                           // unter diesem Satz
+   std::cout << "Dieser See war wunderschön." << std::endl;
 
   /*
    * Fünftes Kapitel
