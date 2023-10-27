71a987102c550405242a8262f1c6642bbe905202
robinpfister <119607482+robinpfister@users.noreply.github.com>
2023-10-27 14:59:55 +0200

    new sentez first chapter

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ int main() {
                "eine Koenigin, die wuenschten sich nichts sehnlicher als nicht "
                "mehr Koenig und Koenigin sein zu muessen."
             << std::endl
+            << "Sie waren auf der Suche nach einem verlorenen Schatz"
+            << std::endl
             << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
                           // unter diesem Satz
 
